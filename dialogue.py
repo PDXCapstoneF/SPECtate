d83a29d7b62c65095fdce922fed4eec8610b7901
Michael Bottini <mbottini1@gmail.com>
2018-04-16 21:58:50 -0700

    Remove obsolete functions from dialogue.py
    All of these removals are from the FIRST BLOOD^(TM) edition,
    where I was still feeling out a few things. Benjamin Spriggs
    implemented a far better version, so it's now unneeded.

--- dialogue.py
+++ dialogue.py
@@ -24,75 +24,6 @@ def read_json(filename):
     with open(filename) as f:
         return json.load(f)
 
-def create_run_dict(runtype, *args):
-    """
-    Returns a dictionary of a run-type option object deserialized
-    from a human-readable configuration file (e.g., JSON, HJSON, YAML).
-    :param runtype: (HBIR || HBIR_RT || PRESET || LOADLEVEL)
-    """
-    runtype_option_dict = read_json('runtype_options.json')
-    if runtype not in runtype_option_dict:
-        return {}
-    parameter_list = runtype_option_dict[runtype]
-
-    if(len(parameter_list) == len(args)):
-        return_dict = {tup[0] : tup[1] for tup in zip(parameter_list, args)}
-        return_dict['Run Type'] = runtype
-        return return_dict
-    return {}
-
-def create_run_dict_list(runtype, args):
-    """
-    Returns a dictionary of run-type option objects.
-    :param runtype: (HBIR || HBIR_RT || PRESET || LOADLEVEL)
-    """
-    return create_run_dict(runtype, *args)
-
-def create_run_sh_invocation(run_dict):
-    """
-    Builds a string to be used to invoke the `run.sh` script in ~/workloads. The
-    command-line arguments of `run.sh` are initialized with the respective values in
-    run_dict.
-    :param run_dict: dict
-    """
-    try:
-        runtype_option_dict = read_json('runtype_options.json')
-        parameters = runtype_option_dict[run_dict['Run Type']]
-        return './run.sh {}'.format(
-            ' '.join(str(run_dict[p]) for p in parameters))
-    except: # Probably a dictionary failure.
-        return ''
-
-
-def execute_run(run_dict):
-    """
-    Invoke the `run.sh` script in ~/workloads with command-line arguments stored
-    in run_dict.
-    :param run_dict: dict
-    """
-    # We'll eventually invoke this for realsies. For now, we're just going to
-    # print it.
-    invocation = create_run_sh_invocation(run_dict)
-    if invocation:
-        print(invocation)
-        return True
-    return False
-
-def execute_runs(run_dict_list):
-    """
-    A wrapper for `execute_run(...)`, which is invoked repeatedly for each set
-    of run-type options found in run_dict_list.
-    :param run_dict_list: [dict]
-    """
-    return [execute_run(run_dict) for run_dict in run_dict_list]
-
-
-
-
-
-
-
-
 # Utility functions.
 
 def print_dict(d):
@@ -100,6 +31,8 @@ def print_dict(d):
         print("{}: {}".format(key, value))
 
 # Level-one layer of dialogue.
+# All functions take run_dict, runtype_dict as arguments so that they can be
+# called homogenously from a dictionary in `dialogue`.
 
 def print_all_runs(run_dict, runtype_dict):
     for k, v in sorted(run_dict.items(), key=lambda x : x[0]):
@@ -222,8 +155,8 @@ def edit_run_dialogue(old_run):
 
 def dialogue():
     """
-    A dialogue function for creating a RunDict.
-    Note that this currently does not allow you to create a new category of Run.
+    A dialogue function for creating, editing, and saving RunSet objects
+    as JSON objects.
     """
 
     default_json = 'example_config.json'
