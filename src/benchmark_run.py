ded05efccd9f18e9a774bbfc5c90a2724cff831a
bspriggs <ben@sprico.com>
2018-04-18 20:09:54 -0700

    Update with some passing tests and check arguments

--- src/benchmark_run.py
+++ src/benchmark_run.py
@@ -14,13 +14,16 @@ class SpecJBBRun:
     Does a run!
     """
 
-    def __init__(self, types={}, props={}):
-        pass
+    def __init__(self, types=None, props=None):
+        if not types or not props:
+            raise InvalidRunConfigurationException
+        self.types = types
+        self.props = props
 
     def run(self):
         pass
 
-    def dump_info(self, level=logging.INFO):
+    def debug(self, logger=log, level=logging.DEBUG):
         """
         Dumps info about this currently configured run.
         """
