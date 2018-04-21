2fffe23457a0e3255d4c6828be343c0582af64b6
bspriggs <ben@sprico.com>
2018-04-21 13:18:39 -0700

    make more sensible with arguments and path

--- tests/test_task_runner.py
+++ tests/test_task_runner.py
@@ -3,28 +3,15 @@ import unittest
 from src.task_runner import TaskRunner
 
 class TestTaskRunner(unittest.TestCase):
-    valid_exec = {
-            'path': '/some/path',
-            'options': []
-            },
-
     valid_options = [
-            valid_exec,
+            "echo",
+            "hello",
+            "world",
             ]
 
     invalid_options = [
-            [ # missing path
-                {
-                    'options': []
-                    },
-                ],
-            [ # missing options
-                {
-                    'path': '/some/path'
-                    },
-                ],
             [ # given non-string arguments
-                valid_exec,
+                "echo",
                 2,
                 None,
                 False,
@@ -32,10 +19,14 @@ class TestTaskRunner(unittest.TestCase):
             ]
 
     def test_init_with_valid_options_works(self):
-        TaskRunner(*self.valid_options)
+        TaskRunner(self.valid_options)
 
     def test_init_with_invalid_options_fails(self):
         with self.assertRaises(Exception):
             for invalid in self.invalid_options:
                 TaskRunner(*invalid)
+
+    def test_run_does_stuff(self):
+        t = TaskRunner("echo", "hello", "world")
+        t.run()
 
