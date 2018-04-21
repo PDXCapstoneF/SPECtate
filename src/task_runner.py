4e618862272dfa3876a8aee20f3465ad6f866a22
bspriggs <ben@sprico.com>
2018-04-20 22:03:46 -0700

    Add basic task runner

---
+++ src/task_runner.py
@@ -0,0 +1,10 @@
+
+class TaskRunner:
+    """
+    Runs a specified task with the given options.
+    """
+    def __init__(self, exec_opts, *extra):
+        pass
+
+    def run(self):
+        pass
