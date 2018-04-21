9fd891c425e282c9fa258e1b51f05c9fc682aacc
bspriggs <ben@sprico.com>
2018-04-21 13:19:19 -0700

    implement passing TaskRunner

--- src/task_runner.py
+++ src/task_runner.py
@@ -1,10 +1,25 @@
+import subprocess
 
 class TaskRunner:
     """
     Runs a specified task with the given options.
     """
-    def __init__(self, exec_opts, *extra):
-        pass
+    def __init__(self, path, *options, **kw):
+        if not path:
+            raise Exception
+
+        self.path = path
+
+        for opt in options:
+            if not isinstance(opt, str):
+                raise Exception
+
+        self.options = list(options)
+        self.kw = kw
+
+
+    def argument_list(self):
+        return [self.path] + self.options
 
     def run(self):
-        pass
+        return subprocess.run(self.argument_list(), stdin=True, check=True, **self.kw)
