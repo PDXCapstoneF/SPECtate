7dd22d43960d3822129431b19380de7f0d8043d8
bspriggs <ben@sprico.com>
2018-04-22 15:41:21 -0700

    Format with spacing

--- tests/test_benchmark_run.py
+++ tests/test_benchmark_run.py
@@ -7,23 +7,23 @@ from src.benchmark_run import SpecJBBRun, TopologyConfiguration, InvalidRunConfi
 
 class TestBenchmarkRun(unittest.TestCase):
     valid_props = {
-        "args": [
-            "Arg1",
-            "Arg2",
-            ],
-        "annotations": {
-            "Arg1": "This is the first argument",
-            "Arg2": "This is the second argument",
-            },
-        "types": {
-            "Arg1": "string",
-            "Arg2": "int",
-            },
-        "translations": {
-            "Arg1": "com.spec.prop1",
-            },
-        "default_props": {},
-        }
+            "args": [
+                "Arg1",
+                "Arg2",
+                ],
+            "annotations": {
+                "Arg1": "This is the first argument",
+                "Arg2": "This is the second argument",
+                },
+            "types": {
+                "Arg1": "string",
+                "Arg2": "int",
+                },
+            "translations": {
+                "Arg1": "com.spec.prop1",
+                },
+            "default_props": {},
+            }
 
     valid_tate_props = {
             "backends": 2,
@@ -31,9 +31,9 @@ class TestBenchmarkRun(unittest.TestCase):
             "jvm": {
                 "path": "/path/to/jvm",
                 "options": [], # to be condensed into a string later
-            },
+                },
             "jar": "/path/to/jar",
-        }
+            }
 
     def test_run_with_empty_config_gives_exception(self):
         with self.assertRaises(InvalidRunConfigurationException):
@@ -52,59 +52,59 @@ class TestBenchmarkRun(unittest.TestCase):
 class TestTopologyConfiguration(unittest.TestCase):
     valid_props = [
             {
-		    "backends": 2,
-		    "injectors": 4,
-		    "controller": ["arg1", "arg2"],
-		    "jvm": "java",
-		    "jar": "env/Main.jar",
+                "backends": 2,
+                "injectors": 4,
+                "controller": ["arg1", "arg2"],
+                "jvm": "java",
+                "jar": "env/Main.jar",
                 },
             {
-		    "backends": {
-                        "count": 2,
-                        "options": ["arg1", "arg2"],
-                        },
-		    "injectors": 4,
-		    "controller": ["arg1", "arg2"],
-		    "jvm": "java",
-		    "jar": "env/Main.jar",
+                "backends": {
+                    "count": 2,
+                    "options": ["arg1", "arg2"],
+                    },
+                "injectors": 4,
+                "controller": ["arg1", "arg2"],
+                "jvm": "java",
+                "jar": "env/Main.jar",
                 },
             {
-		    "backends": 2,
-		    "injectors": {
-                        "count": 4,
-                        "options": ["arg1", "arg2"],
-                        },
-		    "controller": ["arg1", "arg2"],
-		    "jvm": "java",
-		    "jar": "env/Main.jar",
+                "backends": 2,
+                "injectors": {
+                    "count": 4,
+                    "options": ["arg1", "arg2"],
+                    },
+                "controller": ["arg1", "arg2"],
+                "jvm": "java",
+                "jar": "env/Main.jar",
                 },
             {
-		    "backends": 2,
-		    "injectors": 4,
-		    "jvm": "java",
-		    "jar": "env/Main.jar",
+                "backends": 2,
+                "injectors": 4,
+                "jvm": "java",
+                "jar": "env/Main.jar",
                 },
             {
-		    "backends": 2,
-		    "injectors": 4,
-		    "jvm": {
-                        "path": "java",
-                        "options": ["any", "options"],
-                        },
-		    "jar": "env/Main.jar",
+                "backends": 2,
+                "injectors": 4,
+                "jvm": {
+                    "path": "java",
+                    "options": ["any", "options"],
+                    },
+                "jar": "env/Main.jar",
                 },
             ]
 
     invalid_props = [
             {
-		    "injectors": 4,
-		    "jvm": "java",
-		    "jar": "env/Main.jar"
+                "injectors": 4,
+                "jvm": "java",
+                "jar": "env/Main.jar"
                 },
             {
-		    "backends": 4,
-		    "jvm": "java",
-		    "jar": "env/Main.jar"
+                "backends": 4,
+                "jvm": "java",
+                "jar": "env/Main.jar"
                 },
 
             ]
@@ -123,7 +123,7 @@ class TestTopologyConfiguration(unittest.TestCase):
             "injectors": 4,
             "jvm": "java",
             "jar": "env/Main.jar",
-                })
+            })
 
         for f in [t.controller_run_args, t.backend_run_args, t.injector_run_args]:
             self.assertTrue("env/Main.jar" in f())
