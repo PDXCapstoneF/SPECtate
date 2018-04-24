effcbaa30bb2f8463873ed69e08c709b7e864a75
bspriggs <ben@sprico.com>
2018-04-23 22:13:49 -0700

    Add tests for some edge cases

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,9 +1,9 @@
 from schema import SchemaError
 from unittest import TestCase
 import json
-from src.validate import validate, validate_blackbox
+from src.validate import validate, validate_blackbox, TemplateSchema
 
-class TestConfigValidator(TestCase):
+class TestBlackboxConfigValidator(TestCase):
     def test_empty_config_does_not_validate(self):
         with self.assertRaises(SchemaError):
             validate({})
@@ -19,6 +19,7 @@ class TestConfigValidator(TestCase):
                 j = json.loads(f.read())
                 self.assertFalse(validate(j))
 
+class TestSpectateConfigValidator(TestCase):
     def test_example_spectate_config_does_validate(self):
         with open('example_spectate_config.json') as f:
             j = json.loads(f.read())
@@ -30,3 +31,76 @@ class TestConfigValidator(TestCase):
                 j = json.loads(f.read())
                 self.assertFalse(validate(j))
 
+    def test_templates_with_extra_translations_dont_validate(self):
+        self.assertFalse(validate({
+            "templates": {
+                "example": {
+                    "args": [],
+                    "translations": { 
+                        "arg1": "someValue",
+                        },
+                    },
+                },
+            "runs": [],
+            }))
+
+    def test_there_should_be_templates_if_there_are_runs(self):
+        with self.assertRaises(Exception):
+            self.assertFalse(validate({
+                "templates": {},
+                "runs": [
+                    {
+                        "template_name": "NONE",
+                        "args": { "a": "b" },
+                        }
+                    ],
+                }))
+
+    def test_runs_with_extra_args_fail_to_validate(self):
+        with self.assertRaises(Exception):
+            self.assertFalse(validate({
+                "templates": {
+                    "example": {
+                        "args": [],
+                        "translations": { 
+                            "arg1": "someValue",
+                            },
+                        },
+                    },
+                "runs": [
+                    {
+                        "template_name": "example",
+                        "args": {
+                            "a": "b",
+                            "arg1": 5,
+                            },
+                        }
+                    ],
+                }))
+
+    def test_runs_with_ommitted_with_no_defaults_fail_to_validate(self):
+        with self.assertRaises(Exception):
+            self.assertFalse(validate({
+                "templates": {
+                    "example": {
+                        "args": [
+                            "arg1",
+                            "noDefaults",
+                            ],
+                        "translations": { 
+                            "arg1": "someValue",
+                            },
+                    "default_props": {
+                        "arg1": "defaultvalue",
+                        },
+                    },
+                },
+                "runs": [
+                    {
+                        "template_name": "example",
+                        "args": {
+                            "arg1": 5,
+                            },
+                        }
+                    ],
+                }))
