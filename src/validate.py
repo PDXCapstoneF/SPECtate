16e51c5c7298faf6cc2d1fa3d8cd6ec56e15ddfc
bspriggs <ben@sprico.com>
2018-04-23 22:15:22 -0700

    Add passing implementation

--- src/validate.py
+++ src/validate.py
@@ -27,5 +27,73 @@ ConfigSchema = Schema({
 
     })
 
+TemplateSchema = Schema({
+    "args": [is_stringy],
+    Optional("default_props"): {
+        is_stringy: object,
+        },
+    Optional("annotations"): {
+        is_stringy: is_stringy,
+        },
+    Optional("translations"): {
+        is_stringy: is_stringy,
+        },
+    Optional("types"): {
+        is_stringy: is_stringy,
+        },
+    Optional("invocations"): {
+        is_stringy: is_stringy,
+        },
+    })
+
+RunConfigSchema = Schema({
+    "template_name": is_stringy,
+    "args": {
+        Optional(is_stringy): object,
+        },
+    Optional("props_extra"): {
+        Optional(is_stringy): is_stringy,
+        },
+    })
+
+SpectateConfig = Schema({
+    "templates": { 
+        is_stringy: TemplateSchema,
+    },
+    "runs": [RunConfigSchema],
+    })
+
 def validate(unvalidated):
+    d = SpectateConfig.validate(unvalidated)
+
+    # each of the args that appear in the runs,
+    for run in d["runs"]:
+        # for the templates they pull from,
+        t = d["templates"][run["template_name"]]
+
+        # they need to appear in the template
+        for arg in run["args"]:
+            if arg not in t["args"]:
+                return None
+
+        # and if the arg isn't in the run,
+        # it needs to have a default
+        for arg in t["args"]:
+            if arg not in run["args"] and arg not in t["default_props"]:
+                return None
+
+    # for each template,
+    for template in d["templates"].values():
+        # all of the translations need to refer to
+        # arguments specified by the user
+        if "translations" not in template:
+            continue
+
+        for translation in template["translations"]:
+            if translation not in template["args"]:
+                return None
+
+    return d
+
+def validate_blackbox(unvalidated):
     return ConfigSchema.validate(unvalidated)
