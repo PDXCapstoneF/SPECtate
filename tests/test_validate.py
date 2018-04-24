0b59d1e27e00c1c91230234341577982dfb610bd
bspriggs <ben@sprico.com>
2018-04-23 21:39:24 -0700

    add tests for SPECtate specific config validation

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,7 +1,7 @@
 from schema import SchemaError
 from unittest import TestCase
 import json
-from src.validate import validate
+from src.validate import validate, validate_blackbox
 
 class TestConfigValidator(TestCase):
     def test_empty_config_does_not_validate(self):
@@ -11,11 +11,22 @@ class TestConfigValidator(TestCase):
     def test_example_config_does_validate(self):
         with open('example_config.json') as f:
             j = json.loads(f.read())
-            assert 'specjbb' in j
-            self.assertTrue(validate(j))
+            self.assertTrue(validate_blackbox(j))
 
     def test_invalid_config_does_not_validate(self):
         with self.assertRaises(SchemaError):
             with open('tests/fixtures/sample_config_invalid.json') as f:
                 j = json.loads(f.read())
                 self.assertFalse(validate(j))
+
+    def test_example_spectate_config_does_validate(self):
+        with open('example_spectate_config.json') as f:
+            j = json.loads(f.read())
+            self.assertTrue(validate(j))
+
+    def test_invalid_spectate_config_does_not_validate(self):
+        with self.assertRaises(Exception):
+            with open('tests/fixtures/sample_spectate_config_invalid.json') as f:
+                j = json.loads(f.read())
+                self.assertFalse(validate(j))
+
