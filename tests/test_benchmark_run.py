c959ff15843740479348fe20410dc3a7484b3091
bspriggs <ben@sprico.com>
2018-04-25 18:20:39 -0700

    Roll TopologyConfiguration tests into SpecJBB run tests, add relevant keys and test cases

--- tests/test_benchmark_run.py
+++ tests/test_benchmark_run.py
@@ -3,33 +3,9 @@ import json
 import testfixtures
 import logging
 
-from src.benchmark_run import SpecJBBRun, TopologyConfiguration, InvalidRunConfigurationException
+from src.benchmark_run import SpecJBBRun, InvalidRunConfigurationException
 
 class TestBenchmarkRun(unittest.TestCase):
-    valid_props = {
-            "args": [
-                "Arg1",
-                "Arg2",
-                ],
-            "default_props": {},
-            "invocations": "{java} -XX:-AllowUserSignalHandlers {spec}",
-            }
-
-    def test_run_with_empty_config_gives_exception(self):
-        with self.assertRaises(InvalidRunConfigurationException):
-            r = SpecJBBRun()
-
-    def test_run_with_valid_configuration(self):
-        SpecJBBRun(**self.valid_props)
-
-    def test_valid_run_can_dump_info(self):
-        r = SpecJBBRun(**self.valid_props)
-        with testfixtures.LogCapture() as l:
-            r.dump()
-            # need to have some actual logging output
-            self.assertTrue(l.actual())
-
-class TestTopologyConfiguration(unittest.TestCase):
     valid_props = [
             { # multijvm run with counts for each
                 "backends": 2,
@@ -40,6 +16,7 @@ class TestTopologyConfiguration(unittest.TestCase):
                     },
                 "java": "java",
                 "jar": "env/Main.jar",
+                "invocations": "{java} -XX:-AllowUserSignalHandlers {spec}",
                 },
             { # composite run with arguments
                 "controller": {
@@ -48,23 +25,15 @@ class TestTopologyConfiguration(unittest.TestCase):
                     },
                 "java": "java",
                 "jar": "env/Main.jar",
+                "invocations": "{java} -XX:-AllowUserSignalHandlers {spec}",
                 },
-            { # assumed multijvm
-                "backends": 2,
-                "injectors": {
-                    "count": 4,
-                    "options": ["arg1", "arg2"],
-                    },
-                "controller": {
-                    "options": ["arg1", "arg2"],
-                    },
-                "java": "java",
-                "jar": "env/Main.jar",
-                },
-            { # assumed multijvm
+            { # assumed invocations
                 "backends": 2,
                 "injectors": 4,
-                "jvm": {
+                "controller": {
+                    "type": "multi",
+                    },
+                "java": {
                     "path": "java",
                     "options": ["any", "options"],
                     },
@@ -87,18 +56,19 @@ class TestTopologyConfiguration(unittest.TestCase):
 
     def test_valid_props_work(self):
         for valid in self.valid_props:
-            TopologyConfiguration(**valid)
+            SpecJBBRun(**valid)
 
     def test_invalid_props_dont_work(self):
         for invalid in self.invalid_props:
             with self.assertRaises(Exception):
-                TopologyConfiguration(**invalid)
+                SpecJBBRun(**invalid)
 
     def test_valid_give_run_options(self):
-        t = TopologyConfiguration(**{
+        t = SpecJBBRun(**{
             "backends": 2,
             "injectors": 4,
             "controller": {
+                "type": "multi",
                 "options": ["arg1", "arg2"],
                 },
             "java": "java",
@@ -110,3 +80,15 @@ class TestTopologyConfiguration(unittest.TestCase):
             self.assertTrue("java" in f())
             self.assertEqual("java", f()[0])
 
+
+    def test_run_with_empty_config_gives_exception(self):
+        with self.assertRaises(InvalidRunConfigurationException):
+            r = SpecJBBRun()
+
+    def test_valid_run_can_dump_info(self):
+        for valid in self.valid_props:
+            r = SpecJBBRun(**valid)
+            with testfixtures.LogCapture() as l:
+                r.dump()
+                # need to have some actual logging output
+                self.assertTrue(l.actual())
