b22a8a9e215a89a62c128b2ecf8e057e66b5273c
bspriggs <ben@sprico.com>
2018-04-25 19:11:23 -0700

    Add first pass at init

--- src/run_generator.py
+++ src/run_generator.py
@@ -1,2 +1,21 @@
 class RunGenerator:
-    pass
+    def __init__(self, templates=None, runs=None):
+        # let's go ahead and populate everything
+        for run in runs:
+            props = templates.get(run["template_type"])
+
+            # and let's peek for injector count (specjbb.txi.pergroup.count)
+            injectors = props["specjbb.txi.pergroups.count"]
+            # and let's peek for backend count (specjbb.group.count)
+            injectors = props["specjbb.group.count"]
+
+            self.runs.append(**{
+                'controller': None, # TODO: this needs to come from somewhere
+                'backends': backends, 
+                'injectors': injectors,
+                'java': None, # TODO: this needs to come from somewhere
+                'jar': None, # TODO: this needs to come from somewhere
+                'invocations': "{java} {spec}" # TODO: where should this come from?
+            }):
+
+        pass
