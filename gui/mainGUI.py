14a38f5e7eeba2c8410c65f18628a7ae9a49a475
lenhattu <lenhattu12@gmail.com>
2018-04-25 20:38:10 -0700

    new main design and remove multilistbox

--- gui/mainGUI.py
+++ gui/mainGUI.py
@@ -10,13 +10,18 @@ import mainCLI as cli
 with open("properties.json") as properties_file:
     properties = json.load(properties_file)
 
+run_list = ['Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3', 'Run1', 'Run2', 'Run3']
+
 
 class MainWindow(Frame):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self, *args, **kwargs)
+        self.width = properties["main_window"]["width"]
+        self.height = properties["main_window"]["height"]
         self.master.title(properties["program_name"])
         self.master.protocol("WM_DELETE_WINDOW", self.on_close)
-        self.master.minsize(width=properties["main_window"]["width"], height=properties["main_window"]["height"])
+        self.master.minsize(width=self.width, height=self.height)
+        self.master.geometry("%dx%d" % (self.width, self.height))
         menubar = Menu(self.master)
         filemenu = Menu(menubar, tearoff=0)
         filemenu.add_command(label=properties["commands"]["create"], command=self.create_group)
@@ -28,7 +33,45 @@ class MainWindow(Frame):
         menubar.add_cascade(label=properties["commands"]["file"], menu=filemenu)
         self.master.config(menu=menubar)
 
-        list_group = MultiColumnListbox()
+        self.counter = 1
+
+        # Create scroll list
+        self.listbox = Listbox(self, width=20, height=self.height, relief="sunken", font="Arial")
+        self.list_scrollbar = Scrollbar(self)
+        self.listbox.config(yscrollcommand=self.list_scrollbar.set)
+        self.list_scrollbar.config(command=self.listbox.yview)
+        self.listbox.pack(side="left", expand=True, fill="both")
+        self.list_scrollbar.pack(side="left", fill="y")
+        self.listbox.bind("<<ListboxSelect>>", self.on_select)
+
+        # Create canvas
+        self.canvas = Canvas(self, width=80, height=self.height, bg="white", relief="sunken")
+        self.canvas.config(scrollregion=(0, 0, 300, 650), highlightthickness=0)
+        self.canvas.pack(side="left", expand=True, fill="both")
+
+        # Create a dummy text inside canvas
+        self.form = Text(self.canvas, height=self.height, font="Arial")
+        self.form.insert("end", "Click to see...")
+        self.form.pack(expand=True, fill="both")
+
+        # Add items to the listbox
+        i = 0
+        while i < len(run_list):
+            self.listbox.insert(i, run_list[i])
+            i += 1
+
+    def on_select(self, event):
+        selection = event.widget.curselection()
+        if selection:
+            content = event.widget.get(selection[0])
+            if self.counter == 1:
+                self.form.delete(1.0, "end") # CLEAR THE TEXT WHICH IS DISPLAYED AT FIRST RUNNING
+                self.form.insert("end", content)
+                self.counter += 1 #ONE ITEM HAS BEEN SELECTED AND PRINTED SUCCESSFULLY
+            elif self.counter > 1:
+                self.form.delete(1.0, "end")
+                self.form.insert("end", content)
+                self.counter = 1 # RESET THE COUNTER SO THAT NEXT SELECTED ITEM DISPLAYS PROPERLY
 
     def create_group(self):
         # create stuff
@@ -55,5 +98,5 @@ class MainWindow(Frame):
 
 if __name__ == '__main__':
     master = Tk()
-    MainWindow()
+    MainWindow(master).pack(fill="both", expand=True)
     master.mainloop()
