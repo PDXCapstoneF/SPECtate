4561964fa5abf634e9ffcb7280f1440ae27b35c9
bspriggs <ben@sprico.com>
2018-04-26 23:34:56 -0700

    Add props population specified for Tate config, add keys for props and props_file

--- src/run_generator.py
+++ src/run_generator.py
@@ -10,6 +10,14 @@ class RunGenerator:
             template = templates.get(run["template_name"])
             template = TemplateSchema.validate(template)
 
+            # populate default_props
+            props = template.get("default_props", dict()).copy()
+            # populate arguments
+            if "translations" in template:
+                props.update({ translation: run["args"][arg] for arg, translation in template["translations"].items() })
+            if "props_extra" in run:
+                props.update(run["props_extra"])
+
             if "default_props" in template:
                 # and let's peek for injector count (specjbb.txi.pergroup.count)
                 injectors = template["default_props"].get("specjbb.txi.pergroups.count", 1)
@@ -27,4 +35,6 @@ class RunGenerator:
                 'injectors': injectors,
                 'java': template["java"],
                 'jar': template["jar"],
+                'props': props,
+                'props_file': template.get("props_file", 'specjbb2015.props'),
             })
