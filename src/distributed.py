215f994069dcfe1e9bb1dea5eed76c6bde978b50
bspriggs <ben@sprico.com>
2018-05-06 20:27:05 -0700

    Add generated and distributed module that uses generated files

---
+++ src/distributed.py
@@ -0,0 +1,10 @@
+import spectate_pb2_grpc
+
+class SPECtateDistributedRunnerServicer(spectate_pb2_grpc.SPECtateDistributedRunnerServicer):
+    pass
+
+def listen():
+    pass
+
+def run_server():
+    pass
