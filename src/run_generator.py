b7a88dc697a8c9b3952ae3bc9fb942544652c941
bspriggs <ben@sprico.com>
2018-05-07 18:55:48 -0700

    Add logging in run generator

--- src/run_generator.py
+++ src/run_generator.py
@@ -1,15 +1,19 @@
+import logging
 from src.validate import TemplateSchema, RunConfigSchema
 
+log = logging.getLogger(__name__)
 
 class RunGenerator:
     def __init__(self, TemplateData=None, RunList=None):
         self.runs = []
+        log.debug("recieved TemplateData: {}, RunList: {}".format(TemplateData, RunList))
 
         # let's go ahead and populate everything
         for run in RunList:
             run = RunConfigSchema.validate(run)
             template = TemplateData.get(run["template_type"])
             template = TemplateSchema.validate(template)
+            log.debug("run: {}".format(template))
 
             # populate default_props
             props = template.get("default_props", dict()).copy()
@@ -20,16 +24,21 @@ class RunGenerator:
             if "props_extra" in run:
                 props.update(run["props_extra"])
 
-            if "default_props" in template:
+            injectors = 1
+            backends = 1
+
+            if "injectors" in template or "backends" in template:
+                if "injectors" in template:
+                    injectors = template["injectors"]
+                elif "backends" in template:
+                    backends = template["backends"]
+            elif "default_props" in template:
                 # and let's peek for injector count (specjbb.txi.pergroup.count)
                 injectors = template["default_props"].get(
                     "specjbb.txi.pergroups.count", 1)
                 # and let's peek for backend count (specjbb.group.count)
                 backends = template["default_props"].get(
                     "specjbb.group.count", 1)
-            else:
-                injectors = 1
-                backends = 1
 
             self.runs.append({
                 'controller': {
