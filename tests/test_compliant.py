7d994da3d2c80148be2f330579df3f939e87dc21
bspriggs <ben@sprico.com>
2018-05-16 18:26:45 -0700

    Add empty dict validation

--- tests/test_compliant.py
+++ tests/test_compliant.py
@@ -4,6 +4,7 @@ from src import compliant
 class TestComplianceProps(unittest.TestCase):
     def test_defaults_are_compliant(self):
         self.assertTrue(compliant.compliant())
+        self.assertTrue(compliant.compliant({}))
 
     def test_group_count_must_be_compliant(self):
         self.assertFalse(compliant.compliant({
