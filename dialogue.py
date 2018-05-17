13d1782ef43f00b02bb2c32fc0b63fd0c5b7e6fc
Michael Bottini <mbottini1@gmail.com>
2018-05-16 18:14:17 -0700

    Fix delete_run function

--- dialogue.py
+++ dialogue.py
@@ -209,11 +209,11 @@ def create_runtype(run_list, template_dict):
 def delete_run(run_list, template_dict):
     print('Input the tag of the Run that you want to delete. Available tags')
     print('are {}'.format(
-                   ' '.join(run[RUNLIST_ARGS]['Tag'] for run in run_list)))
+                   ' '.join(run[TAG_ARG] for run in run_list)))
     delete_tag = input('-> ')
 
     new_run_list = [run for run in run_list 
-                    if run[RUNLIST_ARGS]['Tag'] != delete_tag]
+                    if run[TAG_ARG] != delete_tag]
     if len(run_list) == len(new_run_list):
         print('Tag {} does not exist.'.format(delete_tag))
     else:
